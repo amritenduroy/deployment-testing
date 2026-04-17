08ed0b592215a061bdc6e75c234560a13b6ff0d9
Amritendu Roy <roy.amritendu@gmail.com>
2026-04-17 11:44:30 +0100

    testing automatic deployment

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return jsonify(message="Hello, World!")
+    return jsonify(message="Hello, World - This is my 1st deployment using github action!")
 
 
 @app.route("/health")
